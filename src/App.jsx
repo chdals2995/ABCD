6c850a8a09afa0af51b3afa08b361baff7f30179
lee-pish <0111183183315v@gmail.com>
2025-11-19 09:54:02 +0900

    Project Created
    ABCD Project File Created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import FakeRealtimeGenerator from "./components/FakeRealtimeGenerator";
+
+function App() {
+  return (
+    <>
+      {/* 상단 헤더 / 라우터 등 기존 내용 */}
+      <FakeRealtimeGenerator />
+      {/* 나머지 페이지들 */}
+    </>
+  );
+}
+
+export default App;
