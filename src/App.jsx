b1dc505433d265c653e7bef174ea397511c52580
lee-pish <0111183183315v@gmail.com>
2025-12-02 14:21:05 +0900

    tailwindcss fontfamily added

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,7 @@ import Button from "./assets/Button";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <Button name={"버튼 테스트"}></Button>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
