438216e8562db7f70d2663f6f67815bbb8c09dd9
lee-pish <0111183183315v@gmail.com>
2025-12-02 14:30:04 +0900

    Button props func,name -> onClick,children

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import { useState } from "react";
 import Button from "./assets/Button";
 
 import "./App.css";
+const onClick = () => {
+  alert("버튼 클릭됨");
+};
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Button onClick={onClick}>qjxms</Button>
+    </>
+  );
 }
 
 export default App;
--- src/assets/Button.jsx
+++ src/assets/Button.jsx
@@ -1,11 +1,11 @@
-export default function Button({ func, name }) {
+export default function Button({ onClick, children }) {
   return (
     <>
       <button
-        onClick={func}
+        onClick={onClick}
         className="w-[79px] h-[47px] rounded-[10px] bg-[#054E76] text-white text-[20px] flex items-center justify-center"
       >
-        {name}
+        {children}
       </button>
     </>
   );
