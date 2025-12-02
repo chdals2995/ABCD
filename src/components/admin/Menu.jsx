11582f3872e2c2286c57b5cfe31218fcb5c7c9ca
chdals2995 <chdals2995@gmail.com>
2025-12-02 15:36:48 +0900

    이미지 폴더 세팅

---
+++ src/components/admin/Menu.jsx
@@ -0,0 +1,12 @@
+export default function Menu(){
+    return(
+        <>
+            <div class='whiteBox'>
+                <div class='blueBox'>
+
+                </div>
+            </div>
+            <div class='tag'></div>
+        </>
+    );
+}
