5a799caf947c59979a2ce66b0e8944fb00f620c2
chdals2995 <chdals2995@gmail.com>
2025-12-04 11:27:18 +0900

    사이트 관리자 최신

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
-import Button from "./assets/Button";
-import Admin from "./layout/AdminLayout";
 import Master from "./pages/Master";
-
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+
 const onClick = () => {
   alert("버튼 클릭됨");
 };
--- src/components/Master/Management.jsx
+++ src/components/Master/Management.jsx
@@ -6,19 +6,24 @@ import Elevator from '../Master/Elevator';
 import { useState } from 'react';
 
 export default function Management(){
-    const [tab, setTab] = useState
+    const [tab, setTab] = useState('A');
 
     return(
         <>
             <div>사이트 관리자</div>
             <div className="m-auto w-[996px]">
                 <ul className="flex items-center justify-center">
-                    <li className="TabMenu w-[333px] h-[82px] border-3 border-[#054E76]">회원관리</li>
-                    <li className="TabMenu w-[333px] h-[82px] border-3 border-[#054E76]">건물등록</li>
-                    <li className="TabMenu w-[333px] h-[82px] border-3 border-[#054E76]">승강기</li>
+                    <li className="TabMenu w-[333px] h-[82px] border-3 border-[#054E76]"
+                        onClick={()=> setTab('A')}>회원관리</li>
+                    <li className="TabMenu w-[333px] h-[82px] border-3 border-[#054E76]"
+                        onClick={()=> setTab('B')}>건물등록</li>
+                    <li className="TabMenu w-[333px] h-[82px] border-3 border-[#054E76]"
+                        onClick={()=> setTab('C')}>승강기</li>
                 </ul>
                 <div className="TabBox w-[996px] h-[546px] border-2">
-                    
+                    {tab === "A" && <Member/>}
+                    {tab === "B" && <Building/>}
+                    {tab === "C" && <Elevator/>}
                 </div>
             </div>
         </>
