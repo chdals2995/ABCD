acea2d56c499dd40e323d1d6c103d7f8d44f0e38
lee_pish <0111183183315v@gmail.com>
2025-12-06 23:43:34 +0900

    완성?

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -7,20 +7,20 @@ import Vacant from "../components/adminpage/Vacant";
 import TestEnergyData from "../components/adminpage/TestEnergyData";
 
 export default function AdminPage() {
-  return (
-    <>
-      <h1 className="font-bold text-[36px] font-pyeojin absolute top-6 left-1/2 -translate-x-1/2 text-[#003C5B] ">
-        건물 관리자
-      </h1>
-      <AdminLayout />
-      <Todo />
-      <div className="w-[1255px] h-[776px] grid grid-cols-[553px_665px] gap-x-[38px] gap-y-[36px] ml-[180px] mt-[60px]">
-        <EnergyData />
-        <MemberList />
-        <Vacant />
-        <Notice />
-      </div>
-      <TestEnergyData />
-    </>
-  );
+   return (
+      <>
+         <h1 className="font-bold text-[36px] font-pyeojin absolute top-6 left-1/2 -translate-x-1/2 text-[#003C5B] ">
+            건물 관리자
+         </h1>
+         <AdminLayout />
+         <Todo />
+         <div className="w-[1255px] h-[776px] grid grid-cols-[553px_665px] gap-x-[38px] gap-y-[36px] ml-[180px] mt-[60px]">
+            <EnergyData />
+            <MemberList />
+            <Vacant />
+            <Notice />
+         </div>
+         {/* <TestEnergyData /> */}
+      </>
+   );
 }
