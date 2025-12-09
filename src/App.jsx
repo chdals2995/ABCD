da3744797cea18674b315c134c96693ebb11e484
chdals2995 <chdals2995@gmail.com>
2025-12-09 15:29:29 +0900

    main fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import { useState } from "react";
 import "./App.css";
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Join from "./pages/login/Join";
 import Login from "./pages/login/Login";
 import AuthStatus from "./components/Login/contexts/AuthStatus";
 import MainPage from "./pages/MainPage";
-
+import { AuthProvider } from "./components/Login/contexts/AuthContext";
 
 function App() {
   return (
     <>
-      <Routes>
-              <Route path="/" element={ <Login/>} />
-              <Route path="/UserMain" element={<AuthStatus/>} />
-              <Route path="/Join" element={ <Join/>} />
-              <Route path="/main" element={<MainPage/>}/>
-            </Routes>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/UserMain" element={<AuthStatus />} />
+            <Route path="/Join" element={<Join />} />
+            <Route path="/main" element={<MainPage />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
 
 export default App;
-        
