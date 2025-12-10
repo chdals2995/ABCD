9552b4d8d00f4e8fd0674d04e19ee98cce8413e5
chdals2995 <chdals2995@gmail.com>
2025-12-10 14:11:53 +0900

    층 분할 계산

--- src/components/main/MainBuilding.jsx
+++ src/components/main/MainBuilding.jsx
@@ -1,8 +1,22 @@
-export default function MainBuilding(){
-    return(
-        <div className=" w-[350px] h-[665px] bg-[url(./assets/imgs/building.png)] bg-cover bg-center
-             ">
+export default function MainBuilding({floors = 10}){
+    const totalHeight = 665;
 
+    // 10층 단위로 몇 칸을 만들지 계산
+    const sectionCount = Math.ceil(floors / 10);
+
+    // 각 층 높이 (px 단위)
+    const floorHeight = totalHeight / sectionCount;
+
+
+    return(
+        <div className="w-[350px] h-[665px] bg-[url(./assets/imgs/building.png)] bg-cover bg-center">
+            {Array.from({ length: floors }).map((_, idx) => (
+                <div
+                    key={idx}
+                    className="border hover:bg-[#054E76] hover:opacity-50"
+                    style={{ height: `${floorHeight}px` }}
+                ></div>
+            ))} 
         </div>
     );
 }
