8f192e373608b5bd0a94847caa862f223df0c122
chdals2995 <chdals2995@gmail.com>
2025-12-12 19:06:05 +0900

    강제 푸쉬

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import MainPage from "./pages/MainPage";
 import { AuthProvider } from "./components/Login/contexts/AuthContext";
 import Floors from "./pages/Floors";
 import AdminPage from "./pages/AdminPage";
+import Master from "./pages/Master";
 import ParkingStatus from "./pages/ParkingStatus";
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
             <Route path="/UserMain" element={<AuthStatus />} />
             <Route path="/Join" element={<Join />} />
             <Route path="/main" element={<MainPage />} />
+            <Route path="/master" element={<Master/>} />
             <Route path="/floors" element={<Floors />} />
             <Route path="/AdminPage" element={<AdminPage />} />
             <Route path="/parking/:lotId" element={<ParkingStatus />} />
