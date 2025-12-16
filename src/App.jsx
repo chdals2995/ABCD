21018eed31903fcfe35cbc77f330a5239c2b9227
jeon-023 <sliirel029@gmail.com>
2025-12-16 10:48:39 +0900

    메인 적용 및 정리함

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import Problems from "./Problems/Problems";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Join from "./pages/login/Join";
 import Login from "./pages/login/Login";
 import AuthStatus from "./components/Login/contexts/AuthStatus";
 import MainPage from "./pages/MainPage";
 import { AuthProvider } from "./components/Login/contexts/AuthContext";
-import Master from "./pages/Master"
+import Floors from "./pages/Floors";
+import AdminPage from "./pages/AdminPage";
+import Master from "./pages/Master";
+import ParkingStatus from "./pages/ParkingStatus";
+import Logout from "./pages/Logout";
+
 
 function App() {
   return (
     <AuthProvider>
-      <Routes>
+      <BrowserRouter>
+       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/UserMain" element={<AuthStatus />} />
         <Route path="/Join" element={<Join />} />
@@ -19,7 +24,8 @@ function App() {
             <Route path="/Master" element={<Master />} />
             
         <Route path="/problems" element={<Problems />} />
-      </Routes>
+       </Routes>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
